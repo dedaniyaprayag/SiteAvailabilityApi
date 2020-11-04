1d1bc20917198c47762288f2f6a3cc6dcd2030e5
dedaniyaprayag <dedaniyaprayag@gmail.com>
2020-11-04 15:44:15 -0500

    Added Unit tests

--- SiteAvailabilityApi.Tests/AvailabilityControllerShould.cs
+++ SiteAvailabilityApi.Tests/AvailabilityControllerShould.cs
@@ -54,5 +54,74 @@ namespace SiteAvailabilityApi.Tests
             Assert.AreEqual(200, okResult.StatusCode);
             _siteAvailablityServiceMock.Verify(x => x.GetSiteHistoryByUser(It.IsAny<string>()), Times.Once);
         }
+
+        [TestMethod]
+        public void PostMessage()
+        {
+            var site = new SiteDto()
+            {
+                Site = "http://www.google.com",
+                Status = true,
+                Timestamp = DateTime.Now,
+                UserId = "123"
+            };
+            // Arrange
+            _siteAvailablityServiceMock.Setup(x => x.SendSiteToQueue(It.IsAny<SiteDto>()));
+
+            // Act
+            var result = _availabilityController.Post(site) as OkResult;
+
+            // assert
+            Assert.AreEqual(200, result.StatusCode);
+            _siteAvailablityServiceMock.Verify(x => x.SendSiteToQueue(It.IsAny<SiteDto>()), Times.Once);
+        }
+
+        [TestMethod]
+        public void ThrowExceptionInPostMessage()
+        {
+            var site = new SiteDto()
+            {
+                Site = "http://www.google.com",
+                Status = true,
+                Timestamp = DateTime.Now,
+                UserId = "123"
+            };
+            // Arrange
+            _siteAvailablityServiceMock.Setup(x => x.SendSiteToQueue(It.IsAny<SiteDto>())).Throws<Exception>();
+
+            // Act
+            var result = _availabilityController.Post(site) as BadRequestObjectResult;
+
+            // assert
+            Assert.AreEqual(400, result.StatusCode);
+            _siteAvailablityServiceMock.Verify(x => x.SendSiteToQueue(It.IsAny<SiteDto>()), Times.Once);
+        }
+
+
+        [TestMethod]
+        public async Task ThrowsExceptionWhileGettingSiteHistoryByUser()
+        {
+            // Arrange
+            var listOfSiteDto = new List<SiteDto>()
+            {
+                new SiteDto()
+                {
+                    Site = "http://www.google.com",
+                    Status= true,
+                    Timestamp = DateTime.Now,
+                    UserId = "123"
+               }
+            };
+            _siteAvailablityServiceMock.Setup(x => x.GetSiteHistoryByUser(It.IsAny<string>())).Throws<Exception>();
+
+            // Act
+            var result = await _availabilityController.GetSiteHistoryByUserAsync("123");
+            var okResult = result as BadRequestObjectResult;
+
+            // assert
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(400, okResult.StatusCode);
+            _siteAvailablityServiceMock.Verify(x => x.GetSiteHistoryByUser(It.IsAny<string>()), Times.Once);
+        }
     }
 }
